3e116218af54672eef434c0b79205df6a8981f06
slava sapir <slavas21@gmail.com>
2025-07-20 16:17:09 -0400

    creating portfolio-featured block

---
+++ template-parts/portfolio-featured-block.php
@@ -0,0 +1,25 @@
+<?php
+   $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+   $link = get_post_meta(get_the_ID(), '_portfolio_external_link', true);
+   $final_link = $link ? esc_url($link) :  home_url();
+?>
+ <a href="<?php echo esc_url($final_link); ?>" class="pfb-card-link" target="_blank" rel="noopener noreferrer">
+            <div class="pfb-card-bgimg" style="background-image:url('<?php echo esc_url($featured_img_url); ?>')">
+                <div class="pfb-card-overlay">
+                    <div class="pfb-card-content">
+                        <!-- <h4 class="pfb-card-subtitle"> -->
+                        <h4>
+                            <?php echo esc_html(get_the_excerpt()); ?>
+                        </h4>
+                        <!-- <h3 class="pfb-card-title"> -->
+                        <h3>
+                            <?php the_title(); ?>
+                        </h3>
+                        <!-- <div class="pfb-card-desc"> -->
+                        <p>
+                            <?php echo apply_filters('the_content', get_the_content()); ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </a>
